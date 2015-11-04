e40d8d76c19cd9b3e6f761630864d5ad931857ec
uerceg <ugljesa@adjust.com>
2015-11-04 13:14:27 +0100

    Setting SDK prefix to xamarin4.0.2

--- Adjust/Adjust.m
+++ Adjust/Adjust.m
@@ -141,7 +141,7 @@ NSString * const ADJEnvironmentProduction   = @"production";
     self.activityHandler = [ADJAdjustFactory activityHandlerWithConfig:adjustConfig
                                         sessionParametersActionsArray:self.sessionParametersActionsArray];
     
-    [adjustConfig setSdkPrefix:@"xamarin4.0.1"];
+    [adjustConfig setSdkPrefix:@"xamarin4.0.2"];
     self.activityHandler = [ADJAdjustFactory activityHandlerWithConfig:adjustConfig];
 }
 
